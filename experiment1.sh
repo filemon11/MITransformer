5a15f5a5d5b303230bd881bb9a05117ffe3ea309
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2024-12-05 14:55:25 +0100

    correction of script

--- experiment1.sh
+++ experiment1.sh
@@ -1,17 +1,19 @@
+#!/bin/sh
+
 N_GPUS=$(lspci|grep -i nvidia | grep -e VGA -e 3D | wc -l)
 # if not usind nvidia gpus, nede to set manually
 N_THREADS=$(nproc --all)
 THREADS_PER_GPU=$((N_THREADS / N_GPUS))
 export OMP_NUM_THREADS=$THREADS_PER_GPU
 
-if [[ $N_GPUS -gt 1 ]]
+if [ $N_GPUS -gt 1 ]
 then
   USE_DDP=1
 else
   USE_DDP=0
 fi
 
-if [[ $N_GPUS = 0 ]]
+if [ $N_GPUS = 0 ]
 then
   DEVICE=cpu
 else
