378b899e8ad5d7d08f42d193a546b5615c7613b8
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2024-12-05 14:38:32 +0100

    automatic gpu und cpu detection in training script

--- experiment1.sh
+++ experiment1.sh
@@ -1,20 +1,39 @@
+N_GPUS=1 #$(lspci|grep -i nvidia | grep -e VGA -e 3D | wc -l)
+# if not usind nvidia gpus, nede to set manually
+N_THREADS=$(nproc --all)
+THREADS_PER_GPU=$((N_THREADS / N_GPUS))
+export OMP_NUM_THREADS=$THREADS_PER_GPU
+
+if [[ $N_GPUS -gt 1 ]]
+then
+  USE_DDP=1
+else
+  USE_DDP=0
+fi
+
+if [[ $N_GPUS = 0 ]]
+then
+  DEVICE=cpu
+else
+  DEVICE=cuda
+fi
 
 prefix="--standalone --nnodes=1 --nproc-per-node=2 main.py"
 
-general_params='--n_workers 32 --device cuda --use_ddp 1 --first_k none --first_k_eval_test none'
+general_params="--n_workers ${THREADS_PER_GPU} --device ${DEVICE} --use_ddp ${USE_DDP} --first_k none --first_k_eval_test none"
 general_hyperopt_params='--batch_size 128 --epochs 100 --early_stop_after 5 --n_trials=50 --eval_interval 100 --n_warmup_steps 500 --n_startup_trials 5 --use_steps 1 --max_steps none'
 hyperopt_selection='--n_embd 200:1000 --dropout 0.0:0.6 --learning_rate 1e-6:1e-2 '
 
 core="${general_params} hyperopt ${hyperopt_selection} ${general_hyperopt_params}"
 
-python main.py --first_k none --first_k_eval_test none dataprep
+# python main.py --first_k none --first_k_eval_test none dataprep
 
 torchrun ${prefix} \
     --name exp1_dep-supervised \
     ${core} \
     --dependency_mode 'supervised' \
     --loss_alpha 0.0:1.0
-    
+
 torchrun ${prefix} \
     --name exp1_dep0 \
     ${core} \
