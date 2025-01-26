b0930e2861937615bfd33384ab7a68fc8147ce03
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2025-01-26 20:24:19 +0100

    adding POS tags and integration costs to RT

--- correlate_RT.sh
+++ correlate_RT.sh
@@ -1,7 +1,12 @@
 model=$1 # "exp1_dep-supervised_det_39"
+baseline=$2
 
-python reading_times.py $model
-
+# python reading_times.py $model
+# python reading_times.py $baseline
+# 
 cd RT
-Rscript --vanilla preproc.R data/words_processed_${model}.csv data/preprocessed_${model}.csv
-Rscript --vanilla analysis.R data/preprocessed_${model}.csv results/plot_${model}.png > results/log_${model}.log
+# Rscript --vanilla preproc.R data/words_processed_${model}.csv data/preprocessed_${model}.csv
+# Rscript --vanilla preproc.R data/words_processed_${baseline}.csv data/preprocessed_${baseline}.csv
+Rscript --vanilla analysis.R data/preprocessed_${model}.csv data/preprocessed_${baseline}.csv results/plot_${model}.png results/plot_${baseline}.pdf > results/log_${model}.log
+
+# sh correlate_RT.sh exp1_dep-alphaselect_0.120_0 exp1_dep-alphaselect_1.0_0
--- reading_times.py
+++ reading_times.py
@@ -5,6 +5,7 @@ Linearity-of-surprisal-on-RT/blob/main/Preparing%20Corpora/get_frequency.py"""
 
 import pandas as pd
 import wordfreq     # type: ignore
+import nltk  # type: ignore
 from tokeniser import TokenMapper
 from data import CoNLLUDataset, TransformMaskHeadChild
 from trainer import LMTrainer
@@ -15,7 +16,8 @@ from natural_stories import load_natural_stories
 import torch
 import numpy as np
 
-from typing import Iterable
+from typing import Iterable, Literal, Callable
+import numpy.typing as npt
 
 import math
 
@@ -37,10 +39,17 @@ LOGFREQ_COL = "logfreq"
 TEXT_ID_COL = "item"
 WNUM_COL = "zone"
 SURPRISAL_COL = "logp"
+WORD_POSITION_COL = "position"
+INTEGRATION_COST_COL = "incost"
+POS_TAG_COL = "POS"
 WLEN_COL = "WLEN"
 
 DEVICE = "cpu"
 BATCH_SIZE = 64
+
+CONTENT_POS = {"FW", "MD", "NN", "NNS", "NNP",
+               "NNPS", "VB", "VBD", "VBG", "VBN",
+               "VBP", "VBZ"}  # , "JJ", "JJR", "JJS"}
 
 
 def get_frequency(token, language: str = LANG):
@@ -86,7 +95,7 @@ def generate_probs(
         trainer: LMTrainer,
         dataset: CoNLLUDataset,
         untokenise: bool = False,
-        surprisal: bool = False) -> torch.Tensor:
+        surprisal: bool = False) -> tuple[torch.Tensor, npt.NDArray[np.int_]]:
     """WARNING: untested"""
 
     pred_probs, _ = trainer.predict(
@@ -96,33 +105,266 @@ def generate_probs(
 
     # Throw away probs of root and eos token
     probs = torch.cat([p[1:-1] for p in pred_probs])
-
-    space_after = np.concat(dataset.space_after)  # type: ignore
+    indices: npt.NDArray[np.int_]
 
     if untokenise:
+        assert dataset.space_after is not None
+        space_after = np.concat(dataset.space_after)
+
         probs = torch.tensor(
             list(untokenise_probs(
                 probs.tolist(),
-                space_after)))
+                space_after,
+                mode="mult")))
+        indices = np.concat(
+            [np.arange(1, sen.sum()+1) for sen in dataset.space_after])
+
+    else:
+        indices = np.concat(
+            [np.arange(1, sen.shape[0]-1) for sen in pred_probs])
+
+    indices = np.append(indices, indices[-1]+1)
 
     if surprisal:
         probs = -torch.log(probs)
 
-    return probs
+    return probs, indices
+
+
+def get_POS_tags(sentence: list[str]) -> list[str]:
+    tagged = nltk.tag.pos_tag(sentence)
+    return [tag for _, tag in tagged]
+
+
+def generate_POS_tags(
+        dataset: CoNLLUDataset,
+        untokenise: bool = False
+        ) -> list[str]:
+    pos_tag_list = []
+    for sentence in dataset.tokens:
+        pos_tags = get_POS_tags(sentence[2:-1])
+        pos_tag_list.extend(pos_tags)
+
+    if untokenise:
+        assert dataset.space_after is not None
+        space_after = np.concat(dataset.space_after)
+
+        new_pos_tag_list = []
+        current_pos = pos_tag_list[0]
+        for i, (_, space_after_tok) in enumerate(
+                zip(pos_tag_list, space_after)):
+            if not space_after_tok:
+                pass
+            else:
+                new_pos_tag_list.append(current_pos)
+                current_pos = pos_tag_list[i+1]
+        new_pos_tag_list.append(current_pos)
+        pos_tag_list = new_pos_tag_list
+
+    return pos_tag_list
+
+
+def get_content_word_mask(
+        sentence: list[str],
+        not_to_mask: set[str] = CONTENT_POS) -> npt.NDArray[np.bool]:
+    pos_tags = get_POS_tags(sentence)
+    print(pos_tags)
+    mask = np.array([tag in not_to_mask for tag in pos_tags], dtype=bool)
+    mask[0:2] = False
+    print(mask)
+    return mask
+
+
+def get_content_word_cost(
+        content_word_mask: npt.NDArray[np.bool],
+        mask_gov: npt.NDArray[np.bool],
+        mask_dep: npt.NDArray[np.bool]
+        ) -> npt.NDArray[np.int_]:
+    print(content_word_mask)
+
+    mask = np.logical_or(mask_gov, mask_dep)
+
+    # do not take into account connections to function words
+    mask[:, ~content_word_mask] = 0
+    mask[0, :] = 0
+
+    print(mask.astype(int))
+    first_connection: npt.NDArray[np.int_] = np.argmax(np.tril(mask), axis=1)
+    print(first_connection)
+
+    upper_triang = np.triu(
+        np.ones((content_word_mask.shape[0], content_word_mask.shape[0])), 1)
+    cw_cumul: np.ndarray = content_word_mask @ upper_triang
+
+    cw_before_first = cw_cumul[first_connection]  # @ mask_gov.T
+    cw_intermediate = cw_cumul - cw_before_first
+    print("first connection:\n", first_connection)
+    print("cw_cumul:\n", cw_cumul)
+    print("cw_before_first:\n", cw_before_first)
+    # assume no crossing arcs
+    # left_cw_child_nums = content_word_mask @ mask_dep.T
+    # cw_intermediate -= left_cw_child_nums
+    print("intermediate:\n", cw_intermediate)
+    cw_intermediate[first_connection == 0] = 0  # if dummy or root
+
+    # cw_intermediate[mask_gov[:, 0]] = 1  # for dummy set 1
+    # cw_intermediate[mask_gov[:, 1]] = 1  # for root set 1
+    print("intermediate:\n", cw_intermediate)
+    print("content words:\n", content_word_mask.astype(int))
+    cost = cw_intermediate + content_word_mask.astype(int)  # new discourse referents
+    cost[~content_word_mask] = 0  # integration and instantiation cost for function words is 0
+    return cost
+
+
+def generate_integration_Gibson(
+        dataset: CoNLLUDataset,
+        untokenise: bool = False,
+        dummy_used: bool = True) -> npt.NDArray[np.int_]:
+    """WARNING: untested"""
+
+    costs_list: list[npt.NDArray[np.int_]] = []
+    heads_list = dataset.heads
+    for i in range(len(dataset)):
+        sentence = dataset[i]
+        # assumes that governor and child mask are called head and child
+        # these are already triangulated
+        mask_gov = sentence["masks"]["head"]
+        mask_dep = sentence["masks"]["child"]
+
+        assert mask_gov is not None and mask_dep is not None
+
+        root_idx = 1
+        mask_dep[:, root_idx] = 0
+        mask_dep[:, 0] = 0      # no cost if no children
+        mask_gov[:, root_idx] = 0
+        mask_gov[:, 0] = 0
+
+        costs = get_content_word_cost(
+            get_content_word_mask(dataset.tokens[i][:-1], CONTENT_POS),
+            mask_gov, mask_dep)
+        print(heads_list[i][:-1])
+        print(costs)
+        print(dataset.tokens[i][:-1])
+        costs_list.append(costs[2:])
+
+    cost_array = np.concat(costs_list)
+    if untokenise:
+        assert dataset.space_after is not None
+        space_after = np.concat(dataset.space_after)
+
+        cost_array = np.array(
+            list(untokenise_probs(
+                cost_array.tolist(),
+                space_after,
+                mode="add")))
+
+    return cost_array
+
+
+def generate_integration_costs(
+        dataset: CoNLLUDataset,
+        untokenise: bool = False,
+        dummy_used: bool = True) -> npt.NDArray[np.int_]:
+    """WARNING: untested"""
+
+    costs_list: list[npt.NDArray[np.int_]] = []
+    for i in range(len(dataset)):
+        sentence = dataset[i]
+        # assumes that governor and child mask are called head and child
+        # these are already triangulated
+        mask_gov = sentence["masks"]["head"]
+        mask_dep = sentence["masks"]["child"]
+
+        assert mask_gov is not None and mask_dep is not None
+
+        root_idx = 1
+        mask_dep[:, root_idx] = 0
+        mask_dep[:, 0] = 0      # no cost if no children
+        mask_gov[:, root_idx] = 0
+        mask_gov[:, 0] = 0
+
+        # restrict this to new content words OR IDEA: do not add if
+        # right head (all intermediate elements have the same head)
+        # has a head left of this element
+
+        mask = mask_gov  # np.logical_or(mask_gov, mask_dep)
+
+        length = mask_gov.shape[0]
+        indices: npt.NDArray[np.int_] = np.tile(
+            np.flip(np.arange(1, length+1)), length).reshape(length, -1)
+        indices = indices - np.flip(
+            np.arange(1, length+1))[..., np.newaxis]
+        #
+        # # change: if no connection at all, add distance + 5
+        # heads = heads_list[i][:-1]
+        # for j, row in zip(range(len(heads)), mask):
+        #     if row[0]:
+        #         delete = True
+        #         current = j+1
+        #         while current < len(heads) and heads[current] == j:
+        #             current += 1
+        #         if current != heads[j]:
+        #             delete = False
+        #         elif heads[current] > j:
+        #             delete = False
+        #
+        #         if delete:
+        #             indices[j, 0] = 1
+        #         else:
+        #             indices[j, 0] = 5  # -= 1  # skip over root node for distance
+        #
+        # # add cost dependent on intermediate nodes without left governor
+        # extra_cost = np.array([0]*len(heads))
+        # num = 0
+        # for j in range(len(heads)):
+        #     if heads[j] > j:
+        #         extra_cost[j] += num
+        #         num += 1
+        #     else:
+        #         num = 0
+        #
+        # # extra_cost += mask_dep[:, 2:].sum(1)
+        #
+        costs = (mask * indices).sum(1) + (mask_gov + mask_dep).sum(-1)
+        costs_list.append(costs[2:])
+
+    cost_array = np.concat(costs_list)
+    if untokenise:
+        assert dataset.space_after is not None
+        space_after = np.concat(dataset.space_after)
+
+        cost_array = np.array(
+            list(untokenise_probs(
+                cost_array.tolist(),
+                space_after,
+                mode="add")))
+
+    return cost_array
 
 
 def untokenise_probs(
         probs: Iterable[float],
         space_after: Iterable[bool],
+        mode: Literal["mult", "add"] = "mult"
         ) -> Iterable[float]:
-    current_prob: float = 1
+    base: float = 0
+    func: Callable[[float, float], float]
+    match mode:
+        case "add":
+            base = 0
+            func = lambda x, y: x+y  # noqa: E731
+        case _:
+            base = 1
+            func = lambda x, y: x*y  # noqa: E731
+
+    current_prob: float = base
     for prob_tok, space_after_tok in zip(probs, space_after):
-        current_prob *= prob_tok
+        current_prob = func(current_prob, prob_tok)
         if not space_after_tok:
             pass
         else:
             yield current_prob
-            current_prob = 1
+            current_prob = base
     yield current_prob
 
 
@@ -150,8 +392,12 @@ def prob_to_surprisal(
 
 def add_surprisal(input_file: str, output_file: str,
                   model_dir: str, token_mapper_dir: str,
+                  dummy_used: bool = True,
                   token_col: str = TOKEN_COL,
                   surprisal_col: str = SURPRISAL_COL,
+                  word_position_col: str = WORD_POSITION_COL,
+                  integration_cost_col: str = INTEGRATION_COST_COL,
+                  pos_tag_col: str = POS_TAG_COL,
                   device: str = DEVICE,
                   batch_size: int = BATCH_SIZE) -> None:
     df = pd.read_csv(input_file)
@@ -174,11 +420,17 @@ def add_surprisal(input_file: str, output_file: str,
                              device=device,
                              use_ddp=False,
                              world_size=1)
-    surprisal = generate_probs(
+    surprisal, indices = generate_probs(
         trainer, dataset,
         untokenise=True, surprisal=True)
 
+    integration_costs = generate_integration_costs(
+        dataset, untokenise=True, dummy_used=dummy_used)
+
     df[surprisal_col] = surprisal
+    df[word_position_col] = indices
+    df[integration_cost_col] = integration_costs
+    df[pos_tag_col] = generate_POS_tags(dataset, untokenise=True)
     df.to_csv(output_file, index=False)
 
 
@@ -186,11 +438,15 @@ def process_tsv(
         input_file: str, output_file: str,
         model_dir: str, token_mapper_dir: str,
         raw: bool = True,
+        dummy_used: bool = True,
         token_col: str = TOKEN_COL,
         text_id_col: str = TEXT_ID_COL,
         wnum_col: str = WNUM_COL,
         logfreq_col: str = LOGFREQ_COL,
         surprisal_col: str = SURPRISAL_COL,
+        word_position_col: str = WORD_POSITION_COL,
+        integration_cost_col: str = INTEGRATION_COST_COL,
+        pos_tag_col: str = POS_TAG_COL,
         wlen_col: str = WLEN_COL,
         language: str = LANG,
         device: str = DEVICE,
@@ -204,8 +460,10 @@ def process_tsv(
     add_word_length(output_file, output_file,
                     token_col, wlen_col)
     add_surprisal(output_file, output_file, model_dir,
-                  token_mapper_dir, token_col,
-                  surprisal_col, device, batch_size)
+                  token_mapper_dir, dummy_used, token_col,
+                  surprisal_col, word_position_col,
+                  integration_cost_col, pos_tag_col,
+                  device, batch_size)
 
 
 if __name__ == "__main__":
