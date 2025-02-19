d61a8b24b7040b9183277981662f503d5b8e417b
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2025-02-20 00:09:16 +0100

    test script adjusted for interval 0.1 to 0.2

--- experiment1_test.sh
+++ experiment1_test.sh
@@ -1,6 +1,14 @@
 # python main.py --first_k none --first_k_eval_test none dataprep
-for i in $(LC_ALL=C seq 0.1 .1 1.0); do
+#for i in $(LC_ALL=C seq 0.1 .1 1.0); do
+#  for j in $(LC_ALL=C seq 0 1 2); do
+#    echo "Testing alpha=${i}, model=${j} ..."
+#    python main.py --use_ddp 0 --device "cuda" --first_k 8 --name test_exp1_dep-alphaselect_${i}_${j} test --model_name exp1_dep-alphaselect_${i}_${j} --batch_size 8 --att_plot 0 --tree_plot 0
+#  done
+#done
+
+for i in $(LC_ALL=C seq 0.105 .005 0.195); do
   for j in $(LC_ALL=C seq 0 1 2); do
+    echo "Testing alpha=${i}, model=${j} ..."
     python main.py --use_ddp 0 --device "cuda" --first_k 8 --name test_exp1_dep-alphaselect_${i}_${j} test --model_name exp1_dep-alphaselect_${i}_${j} --batch_size 8 --att_plot 0 --tree_plot 0
   done
 done
