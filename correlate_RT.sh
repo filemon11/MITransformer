a17d4d8378daf519bbeb13e1401ee0ebe1acb96d
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2025-02-24 15:48:27 +0100

    modified correlation script so that alpha=1.0 is only prepared once

--- correlate_RT.sh
+++ correlate_RT.sh
@@ -6,20 +6,19 @@ prepare_baseline=$4
 for i in $(seq 0 $((model_count -1)))
 do
     python reading_times.py ${model}_${i}
+    cd RT
+    Rscript --vanilla preproc.R data/words_processed_${model}_${i}.csv data/preprocessed_${model}_${i}.csv
+    cd ..
     if [ $prepare_baseline = 1 ]
     then
         python reading_times.py ${baseline}_${i}
-    fi
-done
-cd RT
-for i in $(seq 0 $((model_count -1)))
-do
-    Rscript --vanilla preproc.R data/words_processed_${model}_${i}.csv data/preprocessed_${model}_${i}.csv
-    if [ $prepare_baseline = 1 ]
-    then
+        cd RT
         Rscript --vanilla preproc.R data/words_processed_${baseline}_${i}.csv data/preprocessed_${baseline}_${i}.csv
+        cd ..
     fi
 done
+
+cd RT
 Rscript --vanilla analysis.R ${model} ${baseline} ${model_count} > results/log_${model}.log
 
 # sh correlate_RT.sh exp1_dep-alphaselect_0.120_0 exp1_dep-alphaselect_1.0_0
--- correlate_RT_multiple.sh
+++ correlate_RT_multiple.sh
@@ -1,20 +1,20 @@
 model_count=$1
 
-for i in $(seq 0 $((model_count -1)))
-do
-    python reading_times.py exp1_dep-alphaselect_1.0_${i}
-    cd RT
-    Rscript --vanilla preproc.R data/words_processed_exp1_dep-alphaselect_1.0_${i}.csv data/preprocessed_exp1_dep-alphaselect_1.0_${i}.csv
-    cd ..
-done
+# for i in $(seq 0 $((model_count -1)))
+# do
+#     python reading_times.py exp1_dep-alphaselect_1.0_${i}
+#     cd RT
+#     Rscript --vanilla preproc.R data/words_processed_exp1_dep-alphaselect_1.0_${i}.csv data/preprocessed_exp1_dep-alphaselect_1.0_${i}.csv
+#     cd ..
+# done
 
-for i in $(LC_ALL=C seq 0.1 .1 1.0);
-do
-     echo "Correlating reading times for alpha=${i} ..."
-     sh correlate_RT.sh exp1_dep-alphaselect_${i} exp1_dep-alphaselect_1.0 ${model_count} 0
-done
+# for i in $(LC_ALL=C seq 0.1 .1 1.0);
+# do
+#      echo "Correlating reading times for alpha=${i} ..."
+#      sh correlate_RT.sh exp1_dep-alphaselect_${i} exp1_dep-alphaselect_1.0 ${model_count} 0
+# done
 
-for i in $(LC_ALL=C seq 0.105 .005 0.195);
+for i in 0.105 0.175;  #$(LC_ALL=C seq 0.105 .005 0.195);
 do
      echo "Correlating reading times for alpha=${i} ..."
      sh correlate_RT.sh exp1_dep-alphaselect_${i} exp1_dep-alphaselect_1.0 ${model_count} 0
