e3837233449fb52e71cafc1a91ccfa9f460b53f6
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2025-03-06 13:06:27 +0100

    added fddc and fddw to reading time module

--- reading_times.py
+++ reading_times.py
@@ -8,7 +8,7 @@ import wordfreq     # type: ignore
 import nltk  # type: ignore
 from tokeniser import TokenMapper
 from data import CoNLLUDataset, TransformMaskHeadChild
-from trainer import LMTrainer
+from trainer import LMTrainer, inverse_sigmoid
 from parse import parse_list_of_words_with_spacy
 
 from natural_stories import load_natural_stories
@@ -43,13 +43,15 @@ WORD_POSITION_COL = "position"
 HEAD_DISTANCE_COL = "headdistance"
 INTEGRATION_COST_COL = "incost"
 FDD_COL = "fdd"  # first dependency distance
+FDDC_COL = "fddc"  # first dependency distance correct?
+FDDW_COL = "fddw"  # first dependency distance weight
 LDDS_COL = "ldds"  # left dependency distance sum
 LDC_COL = "ldc"  # left dependency count
 POS_TAG_COL = "POS"
 DEPREL_COL = "deprel"
 WLEN_COL = "WLEN"
 
-DEVICE = "cuda"
+DEVICE = "cpu"
 BATCH_SIZE = 8
 
 TAGSET = None  # "universal"  # None
@@ -70,7 +72,7 @@ if TAGSET is None:
                      "PRP$": "PRP",
                      "UH": "RP",
                      "SYM": "NN"}
-    # in natural stories SYM is assignd e.g. to thirty-two in 1632
+    # in natural stories SYM is assigned e.g. to thirty-two in 1632
 
 else:
     CONTENT_POS = {"NOUN", "VERB"}  # , "JJ", "JJR", "JJS"}
@@ -128,10 +130,11 @@ def generate_probs(
         trainer: LMTrainer,
         dataset: CoNLLUDataset,
         untokenise: bool = False,
-        surprisal: bool = False) -> tuple[torch.Tensor, npt.NDArray[np.int_]]:
+        surprisal: bool = False) -> tuple[torch.Tensor, npt.NDArray[np.int_],
+                                          dict[str, list[torch.Tensor]]]:
     """WARNING: untested"""
 
-    pred_probs, _ = trainer.predict(
+    pred_probs, attention = trainer.predict(
         dataset,
         make_prob=True,
         only_true=True)
@@ -161,7 +164,7 @@ def generate_probs(
     if surprisal:
         probs = -torch.log(probs)
 
-    return probs, indices
+    return probs, indices, attention
 
 
 def get_POS_tags(sentence: list[str]) -> list[str]:
@@ -296,8 +299,8 @@ def generate_first_dep_distance(
         sentence = dataset[i]
         # assumes that governor and child mask are called head and child
         # these are already triangulated
-        mask_gov = sentence["masks"]["head"]
-        mask_dep = sentence["masks"]["child"]
+        mask_gov = sentence["masks"]["head"].copy()
+        mask_dep = sentence["masks"]["child"].copy()
 
         assert mask_gov is not None and mask_dep is not None
 
@@ -335,6 +338,187 @@ def generate_first_dep_distance(
         # which may connect over long distances
         cost_list: list[int] = cost_array.tolist()
         new_cost_list: list[int] = []
+        current_cost: float | None = None
+        found_non_punct: bool = False
+        for space_after_tok, pos, new_cost in zip(
+                space_after, pos_tags, cost_list):
+            if current_cost is None:
+                current_cost = new_cost
+            if not found_non_punct and pos not in PUNCTUATION:
+                current_cost = new_cost
+                found_non_punct = True
+            if space_after_tok:
+                new_cost_list.append(current_cost)
+                found_non_punct = False
+                current_cost = None
+
+        if current_cost is not None:
+            new_cost_list.append(current_cost)
+
+        cost_array = np.array(new_cost_list)
+
+    return cost_array
+
+
+def generate_fddc(
+        dataset: CoNLLUDataset,
+        attention_matrices: dict[str, list[torch.Tensor]],
+        pos_tags: list[str],
+        untokenise: bool = False) -> npt.NDArray[np.bool]:
+    # TODO: all left ones distance sum
+    # TODO: all left ones number
+    """WARNING: untested"""
+
+    fdd = generate_first_dep_distance(dataset, pos_tags, untokenise=False)
+
+    costs_list: list[npt.NDArray[np.bool]] = []
+    elements_in: int = 0
+    for i in range(len(
+            attention_matrices[next(iter(attention_matrices.keys()))])):
+        # assumes that governor and child mask are called head and child
+        # these are already triangulated
+        mask_gov = attention_matrices["head"][i][0].clone()
+        mask_dep = attention_matrices["child"][i][0].clone()
+
+        mask_gov[:, 1] = 0
+        mask_gov[:, 0] = 0
+        mask_dep[:, 1] = 0
+        mask_dep[:, 0] = 0
+
+        mask_gov_ = np.tril(mask_gov) >= 0.5
+        mask_dep_ = np.tril(mask_dep) >= 0.5
+        mask = np.tril(np.logical_or(mask_gov_, mask_dep_))
+
+        first_connection: npt.NDArray[np.int_] = np.argmax(
+            mask, axis=1)
+
+        length = mask_gov.shape[0]
+        indices: npt.NDArray[np.int_] = np.tile(
+            np.flip(np.arange(1, length+1)), length).reshape(length, -1)
+        indices = indices - np.flip(
+            np.arange(1, length+1))[..., np.newaxis]
+
+        distances = -indices[np.arange(0, first_connection.shape[0]),
+                             first_connection]
+        distances[mask.sum(1) == 0] = 0
+        # distance where there is no left element is set to 0
+
+        distances = distances[2:]
+
+        fdd_sen = np.copy(fdd[elements_in:elements_in+distances.shape[0]])
+        fdd_sen[fdd_sen > 0] = 0
+        fddc = fdd_sen == distances
+
+        costs_list.append(fddc)
+        elements_in += distances.shape[0]
+
+    cost_array = np.concat(costs_list)
+
+    if untokenise:
+        assert dataset.space_after is not None
+        space_after = np.concat(dataset.space_after)
+
+        # take the first distance to disregard punctuation
+        # which may connect over long distances
+        cost_list: list[bool] = cost_array.tolist()
+        new_cost_list: list[bool] = []
+        current_cost: bool | None = None
+        found_non_punct: bool = False
+        for space_after_tok, pos, new_cost in zip(
+                space_after, pos_tags, cost_list):
+            if current_cost is None:
+                current_cost = new_cost
+            if not found_non_punct and pos not in PUNCTUATION:
+                current_cost = new_cost
+                found_non_punct = True
+            if space_after_tok:
+                new_cost_list.append(current_cost)
+                found_non_punct = False
+                current_cost = None
+
+        if current_cost is not None:
+            new_cost_list.append(current_cost)
+
+        cost_array = np.array(new_cost_list)
+
+    return cost_array
+
+
+def generate_fddw(
+        dataset: CoNLLUDataset,
+        attention_matrices: dict[str, list[torch.Tensor]],
+        pos_tags: list[str],
+        untokenise: bool = False) -> npt.NDArray[np.bool]:
+    # TODO: all left ones distance sum
+    # TODO: all left ones number
+    """WARNING: untested"""
+
+    costs_list: list[npt.NDArray] = []
+    for i in range(len(
+            attention_matrices[next(iter(attention_matrices.keys()))])):
+        sentence = dataset[i]
+        # assumes that governor and child mask are called head and child
+        # these are already triangulated
+        assert sentence["masks"]["head"] is not None
+        assert sentence["masks"]["child"] is not None
+        mask_gov = sentence["masks"]["head"].copy()
+        mask_dep = sentence["masks"]["child"].copy()
+
+        assert mask_gov is not None and mask_dep is not None
+
+        # throw root connection and future connection together
+        mask_dep[:, 0] = mask_dep[:, 0] + mask_dep[:, 1]
+        mask_dep[:, 1] = 0
+        mask_gov[:, 0] = mask_gov[:, 0] + mask_gov[:, 1]
+        mask_gov[:, 1] = 0
+
+        # first dependent only future dependent
+        # if no left gov and dep dependent
+        mask = np.tril(np.logical_or(mask_gov, mask_dep))
+        mask[:, 0] = np.logical_and(mask_gov[:, 0], mask_dep[:, 0])
+
+        first_connection: npt.NDArray[np.int_] = np.argmax(
+            mask, axis=1)
+
+        # assumes that governor and child mask are called head and child
+        # these are already triangulated
+        mask_gov_pred = attention_matrices["head"][i][0].clone()
+        mask_dep_pred = attention_matrices["child"][i][0].clone()
+
+        # TODO to softmax
+        mask_gov_pred = inverse_sigmoid(mask_gov_pred)
+        mask_gov_pred = mask_gov_pred.softmax(-1)
+        mask_dep_pred = inverse_sigmoid(mask_dep_pred).softmax(-1)
+        mask_dep_pred = mask_dep_pred * mask_dep.sum(1)
+
+        mask_dep_pred[:, 0] = mask_dep_pred[:, 0] + mask_dep_pred[:, 1]
+        mask_dep_pred[:, 1] = 0
+        mask_gov_pred[:, 0] = mask_gov_pred[:, 0] + mask_gov_pred[:, 1]
+        mask_gov_pred[:, 1] = 0
+
+        weights = torch.maximum(
+            mask_gov_pred[
+                np.arange(0, first_connection.shape[0]),
+                first_connection],
+            torch.minimum(
+                mask_dep_pred[
+                    np.arange(0, first_connection.shape[0]),
+                    first_connection],
+                torch.ones(
+                    (1,), device=first_connection.device)))
+        # right weight
+        costs_list.append(weights[2:].detach().numpy())
+
+    cost_array = np.concat(costs_list)
+
+    if untokenise:
+        assert dataset.space_after is not None
+        space_after = np.concat(dataset.space_after)
+
+        # take the first distance to disregard punctuation
+        # which may connect over long distances
+        cost_list: list[float] = cost_array.tolist()
+        new_cost_list: list[float] = []
         current_cost: float | None = None
         found_non_punct: bool = False
         for space_after_tok, pos, new_cost in zip(
@@ -694,6 +878,8 @@ def add_surprisal(input_file: str, output_file: str,
                   pos_tag_col: str = POS_TAG_COL,
                   deprel_col: str = DEPREL_COL,
                   fdd_col: str = FDD_COL,
+                  fddc_col: str = FDDC_COL,
+                  fddw_col: str = FDDW_COL,
                   ldds_col: str = LDDS_COL,
                   ldc_col: str = LDC_COL,
                   device: str = DEVICE,
@@ -718,7 +904,7 @@ def add_surprisal(input_file: str, output_file: str,
                              device=device,
                              use_ddp=False,
                              world_size=1)
-    surprisal, indices = generate_probs(
+    surprisal, indices, attention = generate_probs(
         trainer, dataset,
         untokenise=True, surprisal=True)
 
@@ -730,18 +916,24 @@ def add_surprisal(input_file: str, output_file: str,
         dataset, pos_tag_list, untokenise=True)
 
     fdd = generate_first_dep_distance(dataset, pos_tag_list, untokenise=True)
-
+    fddc = generate_fddc(
+        dataset, attention, pos_tag_list, untokenise=True).astype(int)
+    fddw = generate_fddw(
+        dataset, attention, pos_tag_list, untokenise=True)
     deprels = generate_deprels(dataset, pos_tag_list, untokenise=True)
     ldds = generate_ldds(dataset, pos_tag_list, untokenise=True)
     ldc = generate_ldc(dataset, pos_tag_list, untokenise=True)
 
-    pos_tag_list = generate_POS_tags(pos_tag_list, dataset.space_after,
-                                     untokenise=True)
+    pos_tag_list = generate_POS_tags(
+        pos_tag_list, dataset.space_after,
+        untokenise=True)
 
     df[surprisal_col] = surprisal
     df[word_position_col] = indices
     df[integration_cost_col] = integration_costs
     df[fdd_col] = fdd
+    df[fddc_col] = fddc
+    df[fddw_col] = fddw
     df[ldds_col] = ldds
     df[ldc_col] = ldc
     df[pos_tag_col] = pos_tag_list
@@ -761,6 +953,8 @@ def process_tsv(
         word_position_col: str = WORD_POSITION_COL,
         integration_cost_col: str = INTEGRATION_COST_COL,
         fdd_col: str = FDD_COL,
+        fddc_col: str = FDDC_COL,
+        fddw_col: str = FDDW_COL,
         ldds_col: str = LDDS_COL,
         ldc_col: str = LDC_COL,
         pos_tag_col: str = POS_TAG_COL,
@@ -781,8 +975,8 @@ def process_tsv(
                   token_mapper_dir, token_col,
                   surprisal_col, word_position_col,
                   integration_cost_col, pos_tag_col,
-                  deprel_col, fdd_col, ldds_col,
-                  ldc_col,
+                  deprel_col, fdd_col, fddc_col, fddw_col,
+                  ldds_col, ldc_col,
                   device, batch_size)
 
 
