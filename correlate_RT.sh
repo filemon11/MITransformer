237622204104a99ace5e487937d823cf5a9d68d2
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2025-11-23 17:39:28 +0100

    removed unnecessary content from correlate_RT.sh

--- correlate_RT.sh
+++ correlate_RT.sh
@@ -1,5 +1,5 @@
 corpus=$1
-model=$2 # "exp1_dep-supervised_det_39"
+model=$2
 model_count=$3
 shift=$4
 cost_param=$5
@@ -9,10 +9,7 @@ tokeniser=$7
 for i in $(seq 0 $((model_count -1)))
 do
     python -m mitransformer.readingtimes ${model}_${i} ${corpus} ${shift} ${cost_param} ${left_param} ${tokeniser}
-    # python reading_times.py ${model}_${i} ${corpus}
 done
 
 cd RT
 Rscript --vanilla analysis_new.R ${model} ${model_count} ${corpus} ${shift} > results/log_${model}.log
-
-# sh correlate_RT.sh exp2_lstm_0.120_0 exp2_lstm_1.0_0
