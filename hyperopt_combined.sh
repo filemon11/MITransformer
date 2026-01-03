3ba766d68b4b7cbcad318e90acfb861dcdde3f24
Lukas Mielczarek <lukas.mielczarek@uni-duesseldorf.de>
2026-01-03 03:47:25 +0100

    hyperopt script test

--- hyperopt_combined.sh
+++ hyperopt_combined.sh
@@ -28,18 +28,19 @@ export OMP_NUM_THREADS=$THREADS_PER_GPU
 prefix="--standalone --nnodes=1 --nproc-per-node=${N_GPUS} -m mitransformer.__main__"
 
 general_params="--n_workers ${THREADS_PER_GPU} --device ${DEVICE} --use_ddp ${USE_DDP}"
-general_hyperopt_params='--min_len_train 3 --min_len_eval_test 3 --shift 2 --global_distr 0 --optimise loglik --masked 0 --combined True --losses {lm:none,} --first_k None --first_k_eval_test None --batch_size 38 --gradient_acc 20 --epochs 100 --early_stop_after 4 --n_trials=100 --eval_interval 1000 --n_warmup_steps 2 --use_steps 1 --max_steps none'
-hyperopt_selection='--use_lstm 0 --psyling_dataset geco_train,zuco2_1_train,zuco1_1_train,zuco1_2_train,meco1_train,meco2_train,frank_ET_train --sampler tpe --pruner hyperband --length_weighted 1 --lme_formula GPT~length+position+length.1+length.2+frequency+frequency.1+frequency.2+surprisal+surprisal.1+surprisal.2+(position+length+length.1+length.2+frequency+frequency.1+frequency.2+surprisal+surprisal.1+surprisal.2|WorkerId)+(1|Corpus) --layer_design (0,) --width 8 --depth 8 --n_embd 400 --dropout_attn 0.0 --dropout_resid 0.0 --dropout_ff 0.0 --dropout_embd 0.0 --dropout_lstm 0.0 --learning_rate 1e-3 --d_ff_factor 4 --bias 0 --use_dual_fixed 0'
+general_hyperopt_params='--min_len_train 3 --min_len_eval_test 3 --shift 1 --global_distr 0 --optimise loss --masked 0 --combined False --losses {lm:none,} --first_k None --first_k_eval_test None --batch_size 800 --gradient_acc 1 --epochs 10 --early_stop_after 4 --early_stop_metric loss --n_trials=100 --eval_interval 1000 --n_warmup_steps 4 --use_steps 1 --max_steps none'
+hyperopt_selection='--use_lstm 0 --psyling_dataset geco_train,zuco2_1_train,zuco1_1_train,zuco1_2_train,meco1_train,meco2_train,frank_ET_train --sampler tpe --pruner hyperband --length_weighted 1 --lme_formula GPT~length+position+length.1+frequency+frequency.1+surprisal+surprisal.1+(position+length+length.1+frequency+frequency.1+surprisal+surprisal.1|WorkerId) --layer_design (0,) --width 16 --depth 24 --n_embd 1024 --dropout_attn 0.0 --dropout_resid 0.0;0.5 --dropout_ff 0.0;0.5 --dropout_embd 0.0;0.5 --dropout_lstm 0.0;0.5 --learning_rate 1e-4;1e-3 --d_ff_factor 4;8 --bias 0 --use_dual_fixed 0'
 
 core="${general_params} hyperopt ${hyperopt_selection} ${general_hyperopt_params}"
 
 # geco_train,zuco2_1_train,zuco1_1_train,zuco1_2_train,meco1_train,meco2_train,frank_ET_train
 # naturalstories_train,frank_SP_train
 
+# TODO: allow sentences up to length 60
 # python -m mitransformer dataprep --min_len_train 3 --min_len_eval_test 3 --max_len_train 40 --max_len_eval_test 40 --first_k none --first_k_eval_test none --masked 0
 
 torchrun ${prefix} \
-    --name hyperopt_combined7 \
+    --name standard_test1 \
     ${core} \
     --dependency_mode 'standard'
 
